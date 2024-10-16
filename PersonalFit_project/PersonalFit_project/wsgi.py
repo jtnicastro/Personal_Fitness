39245009d971e0ceaf7dc1eafb5261275b218b83
jtnicastro <jtnicastro@gmail.com>
2024-10-16 17:04:13 -0400

    Added Procfile

--- PersonalFit_project/PersonalFit_project/wsgi.py
+++ PersonalFit_project/PersonalFit_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PersonalFit_project.PersonalFit_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PersonalFit_project.settings')
 
 application = get_wsgi_application()
